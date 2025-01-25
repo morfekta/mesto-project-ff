014c604e25b4f0200b7c974160f9ef7f59e35336
morfekta <taya-al@mail.ru>
2025-01-26 02:07:33 +0300

    добавление карточек и их удаление

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узел для списка карточек
+const cardsList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(cardData, deleteCard) {
+  const card = cardTemplate.cloneNode(true); // Клонируем содержимое темплейта
 
-// @todo: Функция удаления карточки
+  // Заполняем карточку данными
+  card.querySelector('.card__image').src = cardData.link;
+  card.querySelector('.card__image').alt = cardData.name;
+  card.querySelector('.card__title').textContent = cardData.name;
 
-// @todo: Вывести карточки на страницу
+  // Вешаем обработчик на кнопку удаления
+  card.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+  // Возвращаем готовую карточку
+  return card;
+}
+
+// Функция удаления карточки
+function deleteCard(event) {
+  event.target.closest('.card').remove();
+}
+
+// Выводим карточки на страницу
+initialCards.forEach(cardData => {
+  const card = createCard(cardData, deleteCard);
+  cardsList.append(card);
+});
